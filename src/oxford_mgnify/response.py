7a7bd32ddd81bfe292586dd6fbf52af500a46235
MGS-sails <mahfouzshehu@gmail.com>
2025-06-16 04:57:17 +0100

    Undid hardcoded changes

--- src/oxford_mgnify/response.py
+++ src/oxford_mgnify/response.py
@@ -1,20 +1,7 @@
 import requests
-from datetime import datetime
+import json
 
-def get_analysis_from_this_year():
-    current_year = datetime.now().year
-    all_analyses = []
+response = requests.get('https://www.ebi.ac.uk/metagenomics/api/v2/studies/')
+data = json.loads(response.text)
 
-    url = "https://www.ebi.ac.uk/metagenomics/api/v2/analyses/"
-    while url:
-        response = requests.get(url)
-        data = response.json()
-        all_analyses.extend(data["items"])
-        url = data.get("next")    
-
-    analyses_from_this_year = [analysis for analysis in all_analyses if datetime.strptime(analysis['sample']['updated_at'], "%Y-%m-%dT%H:%M:%S.%fZ").year == current_year]
-    latest_analysis_this_year = max(analyses_from_this_year, key=lambda x: x['sample']['updated_at'])
-
-    return latest_analysis_this_year
-
-print(get_analysis_from_this_year())
+print(data)
