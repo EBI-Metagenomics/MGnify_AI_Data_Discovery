e436ab2ee18b9f305361ae419a24e9a614c00147
MGS-sails <mahfouzshehu@gmail.com>
2025-06-16 14:45:44 +0100

    Deployment

--- src/oxford_mgnify/response.py
+++ src/oxford_mgnify/response.py
@@ -1,23 +1,15 @@
 import requests
 import json
-from datetime import datetime, timedelta
 
-def fetch_data_from_query(url):
-    response = requests.get(url)
-    return json.loads(response.text)
+def get_analysis_detail(analysis_accession):
+    response = requests.get(f'https://www.ebi.ac.uk/metagenomics/api/v2/analyses/{analysis_accession}')
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
-data = fetch_data_from_query('https://www.ebi.ac.uk/metagenomics/api/v2/analyses')
-
-last_year = datetime.now() - timedelta(days=365)
-analysis_from_last_year = []
-
-for item in data['items']:
-    if 'sample' in item.keys():
-        sample = item['sample']
-        if sample is not None and 'updated_at' in sample.keys():
-            updated_at = sample['updated_at']
-            updated_date = datetime.strptime(updated_at, '%Y-%m-%dT%H:%M:%S.%fZ')
-            if updated_date > last_year:
-                analysis_from_last_year.append(item)
-
-print(json.dumps(analysis_from_last_year))
+analysis_detail = get_analysis_detail('MGYA03000000004')
+if analysis_detail is not None:
+    print(json.dumps(analysis_detail))
+else:
+    print('Failed to retrieve analysis detail')
