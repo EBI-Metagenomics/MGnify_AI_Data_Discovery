8b3482ffe390012f46538772dd4e103066ebba6b
MGS-sails <mahfouzshehu@gmail.com>
2025-10-13 08:00:48 +0100

    minor debugging of local issues:

--- src/oxford_mgnify/response.py
+++ src/oxford_mgnify/response.py
@@ -1,15 +1,9 @@
 import requests
 import json
 
-def get_analysis_detail(analysis_accession):
-    response = requests.get(f'https://www.ebi.ac.uk/metagenomics/api/v2/analyses/{analysis_accession}')
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+endpoint = "https://www.ebi.ac.uk/metagenomics/api/v2/studies"
 
-analysis_detail = get_analysis_detail('MGYA03000000004')
-if analysis_detail is not None:
-    print(json.dumps(analysis_detail))
-else:
-    print('Failed to retrieve analysis detail')
+response = requests.get(endpoint)
+studies_data = response.json()
+
+print(json.dumps(studies_data))
